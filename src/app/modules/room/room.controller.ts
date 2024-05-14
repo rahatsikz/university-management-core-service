e207d9a03a4e5a94de71269be1cebe11419cc008
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2024-05-14 18:33:04 +0600

    [done] ~ create room api

---
+++ src/app/modules/room/room.controller.ts
@@ -0,0 +1,21 @@
+import { Room } from "@prisma/client";
+import { Request, Response } from "express";
+import httpStatus from "http-status";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { RoomService } from "./room.service";
+
+const insertIntoDB = catchAsync(async(req: Request, res:Response)=> {
+    const result = await RoomService.insertIntoDB(req.body);
+
+    sendResponse<Room>(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Room created successfully",
+        data: result
+    })
+})
+
+export const RoomController = {
+    insertIntoDB
+}
---
+++ src/app/modules/room/room.validation.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+const createRoom = z.object({
+    body: z.object({
+        roomNumber: z.string({
+            required_error: "Room number is required",
+        }),
+        floor: z.string({
+            required_error: "Floor is required",
+        }),
+        buildingId: z.string({
+            required_error: "Building id is required",
+        })
+    })
+})
+
+const updateRoom = z.object({
+    body: z.object({
+        roomNumber: z.string().optional(),
+        floor: z.string().optional(),
+        buildingId: z.string().optional()
+    })
+})
+
+
+export const RoomValidation = {
+    createRoom,
+    updateRoom
+}
+
+
+
+/* 
+  roomNumber String
+  floor      String
+  buildingId String
+
+*/
