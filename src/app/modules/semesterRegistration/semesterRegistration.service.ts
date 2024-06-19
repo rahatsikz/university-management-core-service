26e94e91fbbb7462113cdb444ad03d1798d43f9f
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2024-06-19 10:39:13 +0600

    [done] ~ student payment [transaction error]

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -14,6 +14,7 @@ import { IGenericResponse } from '../../../interfaces/common';
 import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
 import { asyncForEach } from '../../../shared/utils';
+import { StudentSemesterPaymentService } from '../studentSemesterPayment/studentSemesterPayment.service';
 import { StudentSemesterRegistrationCourseService } from '../studentSemesterRegistrationCourse/studentSemesterRegistrationCourse.service';
 import {
   semesterRegistrationRelationalFields,
@@ -417,9 +418,9 @@ const startNewSemester = async (id: string): Promise<{ message: string }> => {
     );
   }
 
-  if (semesterRegistration.academicSemester.isCurrent) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Semester is already started');
-  }
+  // if (semesterRegistration.academicSemester.isCurrent) {
+  //   throw new ApiError(httpStatus.BAD_REQUEST, 'Semester is already started');
+  // }
 
   await prisma.$transaction(async tx => {
     await tx.academicSemester.updateMany({
@@ -455,6 +456,14 @@ const startNewSemester = async (id: string): Promise<{ message: string }> => {
       studentSemesterRegistrations,
       async (studentSemReg: StudentSemesterRegistration) => {
         // console.log(studentSemReg);
+        if (studentSemReg.totalCreditsTaken) {
+          const totalPaymentAmount = studentSemReg.totalCreditsTaken * 5000;
+          await StudentSemesterPaymentService.createSemesterPayment(tx, {
+            studentId: studentSemReg.studentId,
+            academicSemesterId: semesterRegistration.academicSemesterId,
+            totalPaymentAmount: totalPaymentAmount,
+          });
+        }
         const studentSemesterRegistrationCourses =
           await prisma.studentSemesterRegistrationCourse.findMany({
             where: {
---
+++ src/app/modules/studentSemesterPayment/studentSemesterPayment.service.ts
@@ -0,0 +1,46 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+import { DefaultArgs } from '@prisma/client/runtime/library';
+
+const createSemesterPayment = async (
+  prismaClient: Omit<
+    PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>,
+    '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'
+  >,
+  payload: {
+    studentId: string;
+    academicSemesterId: string;
+    totalPaymentAmount: number;
+  }
+) => {
+  //   console.log(payload);
+
+  const isExists = await prismaClient.studentSemesterPayment.findFirst({
+    where: {
+      student: {
+        id: payload.studentId,
+      },
+      academicSemester: {
+        id: payload.academicSemesterId,
+      },
+    },
+  });
+
+  if (!isExists) {
+    const dataToInsert = {
+      studentId: payload.studentId,
+      academicSemesterId: payload.academicSemesterId,
+      fullPaymentAmount: payload.totalPaymentAmount,
+      partialPaymentAmount: payload.totalPaymentAmount * 0.5,
+      totalDueAmount: payload.totalPaymentAmount,
+      totalPaidAmount: 0,
+    };
+
+    await prismaClient.studentSemesterPayment.create({
+      data: dataToInsert,
+    });
+  }
+};
+
+export const StudentSemesterPaymentService = {
+  createSemesterPayment,
+};
