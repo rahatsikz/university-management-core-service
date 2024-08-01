eeccb2cb800ad9e5b0d28343ba150a86b5bf81d0
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2024-08-01 21:36:41 +0600

    student current enrolled course ~ done

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,76 +1,90 @@
-import { Student } from "@prisma/client";
-import { Request, Response } from "express";
-import httpStatus from "http-status";
-import catchAsync from "../../../shared/catchAsync";
-import pick from "../../../shared/pick";
-import sendResponse from "../../../shared/sendResponse";
-import { studentFilterableFields } from "./student.constant";
-import { StudentService } from "./student.service";
+import { Student } from '@prisma/client';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { studentFilterableFields } from './student.constant';
+import { StudentService } from './student.service';
 
-const insertIntoDB = catchAsync(async(req: Request, res:Response)=> {
-    const result = await StudentService.insertIntoDB(req.body);
-    sendResponse<Student>(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Student created successfully",
-        data: result  
-    })
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentService.insertIntoDB(req.body);
+  sendResponse<Student>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student created successfully',
+    data: result,
+  });
+});
 
-})
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const options = pick(req.query, ['sortBy', 'sortOrder', 'limit', 'page']);
+  const filters = pick(req.query, studentFilterableFields);
 
-const getAllFromDB = catchAsync(async(req: Request, res:Response)=> {
-    const options = pick(req.query, ['sortBy', 'sortOrder', 'limit', 'page']);
-    const filters = pick(req.query, studentFilterableFields);
-    
-    const result = await StudentService.getAllFromDB(options, filters);
-    sendResponse<Student[]>(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Students retrieved successfully",
-        meta: result.meta,
-        data: result.data
-    }) 
-})
+  const result = await StudentService.getAllFromDB(options, filters);
+  sendResponse<Student[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Students retrieved successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
 
-const getDataById = catchAsync(async(req: Request, res:Response)=> {
-    const result = await StudentService.getDataById(req.params.id);
-    sendResponse<Student>(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Student retrieved successfully",
-        data: result
-    })
-})
+const getDataById = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentService.getDataById(req.params.id);
+  sendResponse<Student>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student retrieved successfully',
+    data: result,
+  });
+});
 
-const updateIntoDB = catchAsync(async(req: Request, res:Response)=> {
-    const {id} = req.params
-    const payload = req.body
-    
-    const result = await StudentService.updateIntoDB(id, payload);
-    sendResponse<Student>(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Student updated successfully",
-        data: result
-    })
-})
+const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const payload = req.body;
 
-const deleteFromDB = catchAsync(async(req: Request, res:Response)=> {
-    const {id} = req.params;
+  const result = await StudentService.updateIntoDB(id, payload);
+  sendResponse<Student>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student updated successfully',
+    data: result,
+  });
+});
 
-    const result = await StudentService.deleteFromDB(id);
-    sendResponse<Student>(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Student deleted successfully",
-        data: result
-    })
-})
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const result = await StudentService.deleteFromDB(id);
+  sendResponse<Student>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student deleted successfully',
+    data: result,
+  });
+});
+
+const myCourses = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user;
+
+  const filter = pick(req.query, ['academicSemesterId', 'courseId']);
+
+  const result = await StudentService.myCourses(user?.userId, filter);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student course data fetched successfully',
+    data: result,
+  });
+});
 
 export const StudentController = {
-    insertIntoDB,
-    getAllFromDB,
-    getDataById,
-    updateIntoDB,
-    deleteFromDB
-}
+  insertIntoDB,
+  getAllFromDB,
+  getDataById,
+  updateIntoDB,
+  deleteFromDB,
+  myCourses,
+};
--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -7,22 +7,32 @@ import { StudentValidation } from './student.validation';
 
 const router = express.Router();
 
-router.get("/", StudentController.getAllFromDB);
-router.get("/:id", StudentController.getDataById);
+router.get('/', StudentController.getAllFromDB);
+router.get(
+  '/my-courses',
+  auth(ENUM_USER_ROLE.STUDENT),
+  StudentController.myCourses
+);
+router.get('/:id', StudentController.getDataById);
 
-router.post("/",
-auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-validateRequest(StudentValidation.createStudents), 
-StudentController.insertIntoDB);
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(StudentValidation.createStudents),
+  StudentController.insertIntoDB
+);
 
-router.patch("/:id",
-auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-validateRequest(StudentValidation.updateStudent), 
-StudentController.updateIntoDB);
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(StudentValidation.updateStudent),
+  StudentController.updateIntoDB
+);
 
-router.delete("/:id",
-auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-StudentController.deleteFromDB);
-
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  StudentController.deleteFromDB
+);
 
 export const StudentRoutes = router;
